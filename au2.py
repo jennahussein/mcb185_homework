4936224545f13a80e92d142a5c78c1240381d705
username <jenhussein@ucdavis.edu>
2025-02-11 15:31:00 -0800

    update

---
+++ au2.py
@@ -0,0 +1,23 @@
+#AU2 by Jenna Hussein
+
+import math
+
+i = 0
+n = 0
+
+def is_prime(n):
+ for den in range(2, n//2 +1):
+ 	if n % den == 0: return False
+ 	return True
+
+print(is_prime(5))
+
+for i in range(51, 0, -2):
+	if is_prime(n) == True: 
+		print(i, "*")
+	else: None
+	
+if is_prime == True:
+	print(i,"*")
+
+	
