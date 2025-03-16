d4aa11e185839194e9340f33417459802ce10bac
username <jenhussein@ucdavis.edu>
2025-03-15 22:16:28 -0700

    update

---
+++ dogma.py
@@ -0,0 +1,8 @@
+def oligo_mt(seq):
+	count_a = seq.count('A')
+	count_c = seq.count('C')
+	count_g = seq.count('G')
+	count_t = seq.count('T')
+	total = count_a + count_c + count_g + count_t
+	tm_short = (2 * (count_a + count_t)) + (4 * (count_c + count_g))
+	tm_long = 64.9 + (41 * (count_g + count_c - 16.4))/ (total)
